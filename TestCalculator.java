b6c7a30e44f692311c31bad6d36a26c1c9652d3c
MATI UL REHMAN <74356765+Matiz009@users.noreply.github.com>
2021-01-19 21:48:50 +0500

    After exception handling

--- TestCalculator.java
+++ TestCalculator.java
@@ -1,48 +1,56 @@
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TestCalculator {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        calculator calculate=new calculator();
+        Calculator calculate=new Calculator();
         System.out.println("Press 1 to add numbers");
         System.out.println("Press 2 to subtract numbers");
         System.out.println("Press 3 to multiply numbers");
         System.out.println("Press 4 to divide numbers");
         System.out.println("Press 0 to exit");
-        int dec= scanner.nextInt();
-        switch (dec){
-            case 1->{
-                System.out.println("Enter first number");
-                calculate.setNumber1(scanner.nextInt());
-                System.out.println("Enter second number");
-                calculate.setNumber2(scanner.nextInt());
-                System.out.println("Sum of numbers : "+calculator.add(calculate.getNumber1(),calculate.getNumber2()));
-            }
-            case 2->{
-                System.out.println("Enter first number");
-                calculate.setNumber1(scanner.nextInt());
-                System.out.println("Enter second number");
-                calculate.setNumber2(scanner.nextInt());
-                System.out.println("Subtraction of numbers : "+calculator.subtract(calculate.getNumber1(),calculate.getNumber2()));
-            }
-            case 3->{
-                System.out.println("Enter first number");
-                calculate.setNumber1(scanner.nextInt());
-                System.out.println("Enter second number");
-                calculate.setNumber2(scanner.nextInt());
-                System.out.println("Product of numbers : "+calculator.product(calculate.getNumber1(),calculate.getNumber2()));
-            }
-            case 4->{
-                System.out.println("Enter first number");
-                calculate.setNumber1(scanner.nextInt());
-                System.out.println("Enter second number");
-                calculate.setNumber2(scanner.nextInt());
-                System.out.println("Division of numbers is : "+calculator.division(calculate.getNumber1(),calculate.getNumber2()));
+            try {
+                int dec = scanner.nextInt();
+                switch (dec) {
+                    case 1 -> {
+                        System.out.println("Enter first number");
+                        calculate.setNumber1(scanner.nextInt());
+                        System.out.println("Enter second number");
+                        calculate.setNumber2(scanner.nextInt());
+                        System.out.println("Sum of numbers : " + Calculator.add(calculate.getNumber1(), calculate.getNumber2()));
+                    }
+                    case 2 -> {
+                        System.out.println("Enter first number");
+                        calculate.setNumber1(scanner.nextInt());
+                        System.out.println("Enter second number");
+                        calculate.setNumber2(scanner.nextInt());
+                        System.out.println("Subtraction of numbers : " + Calculator.subtract(calculate.getNumber1(), calculate.getNumber2()));
+                    }
+                    case 3 -> {
+                        System.out.println("Enter first number");
+                        calculate.setNumber1(scanner.nextInt());
+                        System.out.println("Enter second number");
+                        calculate.setNumber2(scanner.nextInt());
+                        System.out.println("Product of numbers : " + Calculator.product(calculate.getNumber1(), calculate.getNumber2()));
+                    }
+                    case 4 -> {
+                        System.out.println("Enter first number");
+                        calculate.setNumber1(scanner.nextInt());
+                        System.out.println("Enter second number");
+                        calculate.setNumber2(scanner.nextInt());
+                        System.out.println("Division of numbers is : " + Calculator.division(calculate.getNumber1(), calculate.getNumber2()));
 
+                    }
+                    default -> System.exit(0);
+
+
+                }
+            } catch (InputMismatchException exp) {
+                System.out.println(exp.getMessage());
+                System.out.println("Please provide a valid Input");
             }
-            default -> System.exit(0);
-
-
         }
-    }
+
 }
